a834a162d9e1ace28e0cd1f9fa7a3bab80bea8b5
rafaelribsilva5 <rafaelribsilva5@gmail.com>
2025-03-27 12:12:00 +0000

    FINISHING THE FIRST THREE VIEWS AND RESPECTIVE SELECTS

--- SOURCE/SQL/DB_BUILD/DATA_INSERTION.sql
+++ SOURCE/SQL/DB_BUILD/DATA_INSERTION.sql
@@ -1,3 +1,16 @@
 USE [_MORPHEUS_ENGINE_]
 GO
 
+INSERT INTO SCIENTIFIC_TASK_TYPES (TASK_TYPE_NAME) VALUES 
+('Geological Analysis'),
+('Atmospheric Research'),
+('Biological Experimentation'),
+('Astrophysics Observation'),
+('Radiation Measurement'),
+('Water Sample Analysis'),
+('Seismic Activity Monitoring'),
+('Magnetic Field Analysis'),
+('Soil Composition Study'),
+('Energy Efficiency Testing');
+
+GO
