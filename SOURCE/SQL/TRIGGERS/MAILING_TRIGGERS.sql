d8ffb459d5a07464991459605f8fa317fc1006b4
Eduardo Sá <eduardoxaviersa@gmail.com>
2025-05-22 22:33:52 +0100

    Mailling Trigger Development / SQL SERVER SMTP MAILING SERVICE CONFIGURATION!

--- SOURCE/SQL/TRIGGERS/MAILING_TRIGGERS.sql
+++ SOURCE/SQL/TRIGGERS/MAILING_TRIGGERS.sql
@@ -6,7 +6,7 @@
 USE [_MORPHEUS_ENGINE_] -- DB CONNECTION
 GO
 
-CREATE TRIGGER AA
+ALTER TRIGGER AA
 ON USERS
 AFTER INSERT
 AS
@@ -27,22 +27,41 @@ BEGIN
 
     OPEN _cursor
     FETCH NEXT FROM _cursor INTO @FIRST_NAME, @LAST_NAME, @EMAIL, @PHONENUMBER, @PROFILE_PICTURE_PATH, @COUNTRY_ID
+    
+    --EXEC msdb.dbo.so_create_profile
 
     EXEC msdb.dbo.sp_send_dbmail
-    @profile_name = 'AGENT',
+    @profile_name = 'Web client 1',
     @recipients = @EMAIL,
     @copy_recipients = '',
     @blind_copy_recipients = '',
-    @body = 'Hello, dear ' + @FIRST_NAME + ' ' + @LAST_NAME + ', welcome to MORPHEUS_ENGINE.
-    \n We are glad to have you with us.
-    \nPlease check your profile and update any information if necessary.',
+    @body = 'safinsifjnasfm',
     @subject = 'Welcome to MORPHEUS_ENGINE',
-	@from_address = '<from email>',
-	@reply_to = ''
-    @importance = 'Important',
+	@from_address = 'eduardoxaviersa@gmail.com',
+	@reply_to = '',
+    @importance = 'Normal';
 
     CLOSE _cursor
     DEALLOCATE _cursor
 END
 
 SELECT * FROM USERS (NOLOCK)
+
+INSERT INTO USERS (
+    USER_FIRST_NAME,
+    USER_LAST_NAME,
+    USER_PASSWORD_HASH,
+    USER_EMAIL,
+    USER_PHONENUMBER,
+    USER_PROFILE_PICTURE_PATH,
+    USER_BIRTHDATE,
+    USER_GENDER_ID,
+    USER_COUNTRY_ID,
+    USER_ADRESS,
+    USER_CITIZEN_CARD_NUMBER,
+    USER_SALARY,
+    USER_BANK_ACCOUNT_NUMBER,
+    USER_ACADEMIC_LEVEL_ID,
+    USER_JOB_ID,
+    USER_LEVEL_ID
+) VALUES ('EDUARDO', 'SÁ', 0x4B4B4B, 'eduardoxaviersa@gmail.com', '192168301', 'C:\Pics\EXOS.jpg', '1993-11-11', 1, 1, '112 K St', 1011, 105000, 2011, 8, 2, 4);
