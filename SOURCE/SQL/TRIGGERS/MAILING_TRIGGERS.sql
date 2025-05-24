68596c2fd0cf0a57b3543893c5c4ab3b40e09131
Eduardo Sá <eduardoxaviersa@gmail.com>
2025-05-24 01:28:09 +0100

    TRIGGER AND MAILLING SYSTEMS DEVELOPMENT

--- SOURCE/SQL/TRIGGERS/MAILING_TRIGGERS.sql
+++ SOURCE/SQL/TRIGGERS/MAILING_TRIGGERS.sql
@@ -6,7 +6,7 @@
 USE [_MORPHEUS_ENGINE_] -- DB CONNECTION
 GO
 
-ALTER TRIGGER AA
+CREATE TRIGGER TG_CREATE_USER
 ON USERS
 AFTER INSERT
 AS
@@ -17,51 +17,115 @@ BEGIN
     DECLARE @PHONENUMBER VARCHAR(20)
     DECLARE @PROFILE_PICTURE_PATH VARCHAR(255)
     DECLARE @COUNTRY_ID INT
+    DECLARE @BODY NVARCHAR(MAX)
 
+    --######################################################################
     DECLARE _cursor CURSOR FOR (SELECT USER_FIRST_NAME,
-    USER_LAST_NAME,
-    USER_EMAIL,
-    USER_PHONENUMBER,
-    USER_PROFILE_PICTURE_PATH,
-    USER_COUNTRY_ID FROM inserted (NOLOCK))
+        USER_LAST_NAME,
+        USER_EMAIL,
+        USER_PHONENUMBER,
+        USER_PROFILE_PICTURE_PATH,
+        USER_COUNTRY_ID FROM inserted (NOLOCK))
 
     OPEN _cursor
     FETCH NEXT FROM _cursor INTO @FIRST_NAME, @LAST_NAME, @EMAIL, @PHONENUMBER, @PROFILE_PICTURE_PATH, @COUNTRY_ID
-    
-    --EXEC msdb.dbo.so_create_profile
+    --######################################################################
 
+    SET @BODY = '<html>
+    <body>
+    <h1>Welcome to MORPHEUS_ENGINE!</h1>
+    <p>Dear '
+    SET @BODY = @BODY + @FIRST_NAME + ' ' + @LAST_NAME
+    SET @BODY = @BODY + ',</p>
+    <p>Thank you for registering with us. We are excited to have you on board!</p>
+    <p>Best regards,</p>
+    <p>The MORPHEUS_ENGINE Team</p>
+    </body></html>'
+    
     EXEC msdb.dbo.sp_send_dbmail
-    @profile_name = 'Web client 1',
-    @recipients = @EMAIL,
-    @copy_recipients = '',
-    @blind_copy_recipients = '',
-    @body = 'safinsifjnasfm',
-    @subject = 'Welcome to MORPHEUS_ENGINE',
-	@from_address = 'eduardoxaviersa@gmail.com',
-	@reply_to = '',
-    @importance = 'Normal';
+        @profile_name = 'Web client 1',
+        @recipients = @EMAIL,
+        @copy_recipients = '',
+        @blind_copy_recipients = '',
+        @body_format = 'HTML',
+        @body = @BODY,
+        @subject = 'Welcome to MORPHEUS_ENGINE!',
+        @from_address = 'eduardoxaviersa@gmail.com',
+        @reply_to = '',
+        @importance = 'Normal';
 
     CLOSE _cursor
     DEALLOCATE _cursor
 END
 
-SELECT * FROM USERS (NOLOCK)
+GO
 
-INSERT INTO USERS (
-    USER_FIRST_NAME,
-    USER_LAST_NAME,
-    USER_PASSWORD_HASH,
-    USER_EMAIL,
-    USER_PHONENUMBER,
-    USER_PROFILE_PICTURE_PATH,
-    USER_BIRTHDATE,
-    USER_GENDER_ID,
-    USER_COUNTRY_ID,
-    USER_ADRESS,
-    USER_CITIZEN_CARD_NUMBER,
-    USER_SALARY,
-    USER_BANK_ACCOUNT_NUMBER,
-    USER_ACADEMIC_LEVEL_ID,
-    USER_JOB_ID,
-    USER_LEVEL_ID
-) VALUES ('EDUARDO', 'SÁ', 0x4B4B4B, 'eduardoxaviersa@gmail.com', '192168301', 'C:\Pics\EXOS.jpg', '1993-11-11', 1, 1, '112 K St', 1011, 105000, 2011, 8, 2, 4);
+CREATE TRIGGER TG_POWER_PLANT_MAINTENANCE_REPORT
+ON POWER_PLANT_MAINTENANCE_LOGS
+AFTER INSERT
+AS
+BEGIN
+    -----------------------------------------------------------------------
+    DECLARE @LOG_ID INT
+    DECLARE @OPERATION_ID INT
+    DECLARE @POWER_PLANT_ID INT
+    DECLARE @MAINTENANCE_DESCRIPTION VARCHAR(MAX)
+    DECLARE @MAINTENANCE_DATE DATE
+    DECLARE @MAINTENANCE_TECHNITIAN INT
+    DECLARE @MAINTENANCE_RESULTS_INFO VARBINARY(255)
+    -----------------------------------------------------------------------
+    DECLARE @POWER_PLANT_NAME VARCHAR(50)
+    -----------------------------------------------------------------------
+    DECLARE @SUBJECT VARCHAR(255)
+    DECLARE @BODY VARCHAR(MAX)
+    DECLARE @EMAIL VARCHAR(100)
+    
+    --######################################################################
+    DECLARE _cursor CURSOR FOR (SELECT LOG_ID,
+        OPERATION_ID,
+        POWER_PLANT_ID,
+        MAINTENANCE_DESCRIPTION,
+        MAINTENANCE_DATE,
+        MAINTENANCE_TECHNITIAN_ID,
+        MAINTENANCE_RESULTS_INFO FROM inserted (NOLOCK))
+
+    OPEN _cursor
+    FETCH NEXT FROM _cursor INTO @LOG_ID, @OPERATION_ID, @POWER_PLANT_ID, @MAINTENANCE_DESCRIPTION, @MAINTENANCE_DATE, @MAINTENANCE_TECHNITIAN, @MAINTENANCE_RESULTS_INFO
+    --######################################################################
+
+    SET @POWER_PLANT_NAME = (SELECT POWER_PLANT_NAME
+        FROM POWER_PLANTS (NOLOCK)
+        WHERE POWER_PLANT_ID = @POWER_PLANT_ID)
+    SET @SUBJECT = 'Maintenace Report for <' + CAST(@POWER_PLANT_NAME AS VARCHAR) + '> Power Plant'
+
+    SET @BODY = '<html>
+    <body>
+    <h1>Power Plant Maintenance Report</h1>
+    <p>Dear Team,</p>
+    <p>Maintenance done in the following power plant</p>
+    <p>Details:</p>
+    <ul>
+        <li>Log ID: '
+    SET @BODY = @BODY + CAST(@LOG_ID AS VARCHAR) + '</li>'
+
+    SET @EMAIL = (SELECT USERS.USER_EMAIL
+        FROM POWER_PLANTS (NOLOCK)
+        INNER JOIN COMPANIES ON POWER_PLANTS.POWER_PLANT_COMPANY_ID = COMPANIES.COMPANY_ID
+        INNER JOIN USERS ON COMPANIES.COMPANY_CEO_ID = USERS.USER_ID
+        WHERE POWER_PLANTS.POWER_PLANT_ID = @POWER_PLANT_ID)
+
+    EXEC msdb.dbo.sp_send_dbmail
+        @profile_name = 'Web client 1',
+        @recipients = @EMAIL,
+        @copy_recipients = '',
+        @blind_copy_recipients = '',
+        @body_format = 'HTML',
+        @body = @BODY,
+        @subject = @SUBJECT,
+        @from_address = 'eduardoxaviersa@gmail.com',
+        @reply_to = '',
+        @importance = 'Normal';
+
+    CLOSE _cursor
+    DEALLOCATE _cursor
+END
